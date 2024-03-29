aad1f4f83157a4bbc0eff40455a13d4111c696d6
nyambujoy <joyliznyambura@gmail.com>
2024-03-29 13:36:32 +0300

    added settimeinterval

--- anotherImageSlider/app.js
+++ anotherImageSlider/app.js
@@ -2,23 +2,38 @@ const container = document.querySelector('.container')
 const images = Array.from(container.querySelectorAll('.images'))
 // console.log(images[1])
 const navButtons = document.querySelectorAll('.span-nav-buttons')
+let currentIndex = 0
 
 
+
+
+
+
+function showNextImage() {
+    images[currentIndex].classList.remove('images--selected')
+    navButtons[currentIndex].classList.remove('span-nav-buttons--selected')
+
+
+    // Increment the index or loop back to the beginning if reached the end
+    currentIndex = (currentIndex + 1) % images.length
+
+    images[currentIndex].classList.add('images--selected')
+    navButtons[currentIndex].classList.add('span-nav-buttons--selected')
+
+}
+
+function setAutochange() {
+    setInterval(showNextImage, 3000)
+}
 
 navButtons.forEach((button, i) => {
     button.addEventListener('click', () => {
-        // console.log(index)
-        images.forEach(image => {
-            image.classList.remove('images--selected')
-            navButtons.forEach(button => button.classList.remove('span-nav-buttons--selected'))
-
-            images[i].classList.add('images--selected')
-            button.classList.add('span-nav-buttons--selected')
-        })
+        currentIndex = i
+        showNextImage()
 
     })
 
 
 })
 
-
+setAutochange()
